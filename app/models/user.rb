976a273e58146e52e4966bd80b79983db1ab2707
pipime <hajimebenjian8@gmail.com>
2019-07-16 05:43:17 +0000

    ヘッダー機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :reviews, dependent: :destroy
-  has_many :likes, dependent: :destroy
+  has_many :reviews,   dependent: :destroy
+  has_many :likes,     dependent: :destroy
+
+  validates :name,     presence: true
+  validates :email,    presence: true, uniqueness: true
 end
